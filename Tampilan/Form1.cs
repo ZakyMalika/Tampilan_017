063185c61a233b29d7b1ea849be8c264fed81446
ZakyMalika <zakymalika0987@gmail.com>
2025-03-20 18:40:54 +0700

    fungsi button delete

--- Tampilan/Form1.cs
+++ Tampilan/Form1.cs
@@ -108,7 +108,7 @@ namespace Tampilan
             }
         }
 
-        private void btnDelete_Click(object sender, EventArgs e)
+        private void btnDelete_Click(object sender, EventArgs e) 
         {
             if (dgvMahasiswa.SelectedRows.Count > 0)
             {
